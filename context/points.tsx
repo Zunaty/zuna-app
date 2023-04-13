3171330b42d6c864fffef1373079f012a40d820b
Victor Perez <Zunaty@gmail.com>
2023-04-12 22:01:41 -0600

    Update to points context

--- context/points.tsx
+++ context/points.tsx
@@ -12,23 +12,99 @@ type PointsProviderProps = {
   children: React.ReactNode;
 };
 
+interface UserData {
+  totalPoints: number;
+  navLink: {
+    logoClicked: boolean;
+    homeClicked: boolean;
+    aboutClicked: boolean;
+    projectsClicked: boolean;
+  };
+  themeSwitchClicked: boolean;
+  projects: {
+    project1Clicked: boolean;
+    project2Clicked: boolean;
+    project3Clicked: boolean;
+  };
+  skills: {
+    searchClicked: boolean;
+  };
+}
+
 export const PointsProvider = ({ children }: PointsProviderProps) => {
-  const [points, setPoints] = useState<number>(() => {
-    const savedPoints =
-      typeof window !== "undefined" ? localStorage.getItem("points") : null;
-    return savedPoints ? parseInt(savedPoints, 10) : 0;
+  const [userData, setUserData] = useState<UserData>(() => {
+    const savedUserData =
+      typeof window !== "undefined" ? localStorage.getItem("userData") : null;
+
+    if (savedUserData) {
+      return JSON.parse(savedUserData);
+    } else {
+      return {
+        totalPoints: 0,
+        navLink: {
+          logoClicked: false,
+          homeClicked: false,
+          aboutClicked: false,
+          projectsClicked: false,
+        },
+        themeSwitchClicked: false,
+        projects: {
+          project1Clicked: false,
+          project2Clicked: false,
+          project3Clicked: false,
+        },
+        skills: {
+          searchClicked: false,
+        },
+      };
+    }
   });
 
   useEffect(() => {
-    localStorage.setItem("points", points.toString());
-  }, [points]);
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }, [userData]);
 
   const addPoints = (amount: number) => {
-    setPoints((prevPoints) => prevPoints + amount);
+    setUserData((prevUserData) => {
+      const newTotalPoints = prevUserData.totalPoints + amount;
+
+      return {
+        ...prevUserData,
+        totalPoints: newTotalPoints,
+      };
+    });
   };
 
+  useEffect(() => {
+    let numberOfClicks = 0;
+
+    for (const key in userData) {
+      if (typeof userData[key] === "boolean" && userData[key] === true) {
+        numberOfClicks += 1;
+      } else if (typeof userData[key] === "object") {
+        for (const subKey in userData[key]) {
+          if (
+            typeof userData[key][subKey] === "boolean" &&
+            userData[key][subKey] === true
+          ) {
+            numberOfClicks += 1;
+          }
+        }
+      }
+    }
+
+    setUserData((prevUserData) => {
+      const newTotalPoints = prevUserData.totalPoints + numberOfClicks;
+
+      return {
+        ...prevUserData,
+        totalPoints: newTotalPoints,
+      };
+    });
+  }, []);
+
   return (
-    <PointsContext.Provider value={{ points, addPoints }}>
+    <PointsContext.Provider value={{ points: userData.totalPoints, addPoints }}>
       {children}
     </PointsContext.Provider>
   );
